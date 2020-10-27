4005f4cbd73dd1ce0847981cf31f6dd50c25d4db
rifath33 <hrifath20@stuy.edu>
2020-10-27 19:14:08 -0400

    Task #1... done

---
+++ BankAccount.java
@@ -0,0 +1,57 @@
+public class BankAccount{
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+
+//  1. A constructor: Should accept values for accountID and password, the
+//  balance should be set to 0.
+
+  public BankAccount(int accountID, String password){
+    balance = 0;
+  }
+
+/*
+  2. get methods for balance, accountID. (just return the value!)
+
+  3. set method for password
+
+  4a. A method to deposit money into the account.
+    public boolean deposit(double amount){...}
+    It should accept an amount of money.
+
+   When the amount is non-negative increase the account balance by that much,
+   return true.
+
+   When the argument given is a negative you should leave the balance
+   unchanged and return false.
+
+   4b. Method to withdraw money from the account.
+   public boolean withdraw(double amount){... }
+
+    This will not be successful if the balance is too low,
+    or the amount is negative. In these cases do not change the balance.
+
+    When the amount is non-negative subtract it from the balance and
+    return true if it was successful,false otherwise.
+
+    5. A toString() method that shows us the AccountID and balance
+    in the format:   "ID\tBALANCE" e.g. "5213452\t99.423"
+
+    6. A main method (in a separate driver file) to test your code.
+*/
+
+}
+
+/*
+  Since withdraw and deposit return booleans, we can use them as follows:
+  (maybe add the balance so you see the updated value):
+
+  if( var.withdraw(1000) ){
+            println("Withdrawal success!");
+       }else{
+            println("Withdrawal failure");
+       }
+
+*/
---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+  public static void main(String[] args){
+
+    BankAccount Rifath = new BankAccount(218964815, "haha");
+
+
+  }
+}
