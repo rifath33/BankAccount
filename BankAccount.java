a12f10dc66f79136bb14c95273ff9f7bbc92f4d5
rifath33 <hrifath20@stuy.edu>
2020-10-29 23:56:07 -0400

    Finished Part 1 for hw13

--- BankAccount.java
+++ BankAccount.java
@@ -5,8 +5,8 @@ public class BankAccount{
   private String password;
 
 
-//  1. A constructor: Should accept values for accountID and password, the
-//  balance should be set to 0.
+  //  1. A constructor: Should accept values for accountID and password, the
+  //  balance should be set to 0.
 
   public BankAccount(int ID, String pass){
     balance = 0;
@@ -15,7 +15,7 @@ public class BankAccount{
   }
 
 
-// 2. get methods for balance, accountID. (just return the value!)
+  // 2. get methods for balance, accountID. (just return the value!)
 
   public double getBalance(){
     return balance;
@@ -26,7 +26,7 @@ public class BankAccount{
   }
 
 
-// 3. set method for password
+  // 3. set method for password
 
   public void setPassword(String newPass){
     password = newPass;
@@ -37,15 +37,15 @@ public class BankAccount{
   }
 
 
-// 4a. A method to deposit money into the account.
-//   public boolean deposit(double amount){...}
-//   It should accept an amount of money.
-//
-//  When the amount is non-negative increase the account balance by that much,
-//  return true.
-//
-//  When the argument given is a negative you should leave the balance
-//  unchanged and return false.
+  // 4a. A method to deposit money into the account.
+  //   public boolean deposit(double amount){...}
+  //   It should accept an amount of money.
+  //
+  //  When the amount is non-negative increase the account balance by that much,
+  //  return true.
+  //
+  //  When the argument given is a negative you should leave the balance
+  //  unchanged and return false.
 
   public boolean deposit(double amount){
     if(amount >= 0){
@@ -68,7 +68,7 @@ public class BankAccount{
    // return true if it was successful,false otherwise.
 
    public boolean withdraw(double amount){
-     if( (balance > amount) && (amount >= 0) ){
+     if( (balance >= amount) && (amount >= 0) ){
        balance = balance - amount;
        return true;
      }
@@ -81,8 +81,34 @@ public class BankAccount{
 
    public String toString(){
      return (
-     Integer.toString(accountID) + "\\t" + Double.toString(balance)
+     "#" + accountID + "\t$" + Double.toString(balance)
      );
    }
+
+   /////////////////////////////////////////////////////////////////////////////
+   // 2020-10-29
+
+  // This private method will allow internal methods to check the password easily
+  // Determine if the password in the object is the same as the parameter.
+  // *@param password to be checked against the object's password (remember the distinction between this.x and x)
+  // @return true if the passwords are the same, false otherwise.
+
+  private boolean authenticate(String pass){
+    return (password.equals(pass));
+  }
+
+  // *Transfer money from this BankAccount to the other only when the password matches, and the withdrawal succeeds.
+  // *@param other which BankAccount to GIVE the money to
+  // *@param amount how much money to transfer, negative amounts will make this operation fail.
+  // *@param password to be checked against the source account, incorrect password will make this operation fail.
+  // *@return true if money is successfully transfered, false otherwise.
+
+  public boolean transferTo(BankAccount other, double amount, String password){
+    if((withdraw(amount) == true) && (authenticate(password)==true) && (amount>=0)){
+      other.balance = other.balance + amount;
+      return true;
+    }
+    else return false;
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -3,22 +3,21 @@ public class Tester{
 
     BankAccount Rifath = new BankAccount(218964815, "haha");
 
-    System.out.println(Rifath.getBalance());
+    BankAccount Bekim = new BankAccount(54, "kutha");
 
-    System.out.println(Rifath.getAccountID());
+    System.out.println(Rifath.transferTo(Bekim, 14, "haha"));
 
-    System.out.println(Rifath.getPassword());
-
-    Rifath.setPassword("kutha");
-
-    System.out.println(Rifath.getPassword());
-
-    Rifath.deposit(1590);
-
-    Rifath.withdraw(15.91);
-
+    Rifath.deposit(14);
 
     System.out.println(Rifath.toString());
+
+    System.out.println(Rifath.transferTo(Bekim, 14, "haha"));
+
+    System.out.println(Bekim.toString());
+
+    System.out.println(Rifath.toString());
+
+
 
   }
 }
